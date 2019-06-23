c2bf18150fc43b41cb655928e37cd4c8d437f575
EC2 Default User <ec2-user@ip-172-31-46-216.ap-northeast-1.compute.internal>
2019-06-23 06:01:20 +0000

    イベントコントローラー　ストロングパラメーター作成

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,14 @@
 class EventsController < ApplicationController
     def new
+    end
+    
+    def create
+     Event.create(event_params)
+    end
+    
+    private
+    def event_params
+        params.permit(:image,:name,:place,:price,:text,:scedule)
         
     end
 end
