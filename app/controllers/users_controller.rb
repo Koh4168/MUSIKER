23323406bed3c4a4a51937ecc01961852a41bd40
EC2 Default User <ec2-user@ip-172-31-46-216.ap-northeast-1.compute.internal>
2019-06-24 09:08:38 +0000

    ページネーション作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
     def show
         
         @name=current_user.name
-        @events= Event.where(user_id: current_user.id)
+        @events= Event.where(user_id: current_user.id).page(params[:page]).per(5)
     end
 end
