3a5caf331917d72cd2622f993dfcb62f6288c7ea
EC2 Default User <ec2-user@ip-172-31-46-216.ap-northeast-1.compute.internal>
2019-06-24 07:51:14 +0000

    usersコントローラに@nmae,@events追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
     def show
-        
-        
+        @name=current_user.name
+        @events= Events.where(user_id: current_user.id).page(params[:page]).per(10).oreder(created_at_DESC)
     end
 end
