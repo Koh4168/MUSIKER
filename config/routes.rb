9d1759176ad8b01dfbe5e833f7756f4d18b667d1
EC2 Default User <ec2-user@ip-172-31-46-216.ap-northeast-1.compute.internal>
2019-06-27 08:05:44 +0000

    mypageから投稿する際、usersが残ったままになったため、／users／events／newルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
      root to: "top#index"
      get "events"           => "events#index"
+     get "/users/events/new" => "events#new"
      get "events/new"       => "events#new"
      post "events"          => "events#create"
      delete "events/:id"    => "events#destroy"
