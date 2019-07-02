77d1527cad8f8a81d2dbf1e48b4e57e209e247af
EC2 Default User <ec2-user@ip-172-31-46-216.ap-northeast-1.compute.internal>
2019-07-02 06:20:06 +0000

    users/edit ルーティング　

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
      get "events/:id/edit"  => "events#edit"
      get  "users/:id"       => "users#show"
      get "events/:id"       => "events#show"
+     get 'users/:id/edit'   =>  "users#edit"
      end
