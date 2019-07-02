2ff56ce18e9aaeb19c525958b2d5cd7fa3b54b9c
EC2 Default User <ec2-user@ip-172-31-46-216.ap-northeast-1.compute.internal>
2019-07-02 10:45:42 +0000

    会員登録の際にimageのvalidates設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
   has_many :events
   mount_uploader :image, ImageUploader
+  validates :image, presence: true
 end
