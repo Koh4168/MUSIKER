07ae08e0cb718f80e793df29c0441523e94b7689
EC2 Default User <ec2-user@ip-172-31-46-216.ap-northeast-1.compute.internal>
2019-07-04 11:05:37 +0000

    ユーザー情報editビュー実装、updateビュー実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,20 @@ class UsersController < ApplicationController
     def edit
         @user=User.find(params[:id])
     end
+    
+    def update
+        @user=User.find(params[:id])
+        @event = Event.find(params[:id]) #event内のuser_idを利用
+        if @event.user_id == current_user.id　#event内のuser_idとusers内のidは同じ
+            @user.update(user_params)
+        end
+        unless @user.save
+        render action: :edit
+        end 
+    end
+    
+    private
+    def user_params
+       params.require(:user) .permit(:image,:name,:text,:link).merge(user_id: current_user.id)
+    end
 end
