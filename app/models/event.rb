c7b8ec708324ea6461336c463b628b0972fffa5b
EC2 Default User <ec2-user@ip-172-31-46-216.ap-northeast-1.compute.internal>
2019-07-13 09:17:07 +0000

    save_groopings 記述

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,6 +4,12 @@ class Event < ApplicationRecord
     has_many :event_groopings, dependent: :destroy
     mount_uploader :image, ImageUploader
     validates :image,:name,:place,:text,:link,:scedule, presence: true
+    
+    def save_groopings(tags)
+        curent_tags = self.groopings.pluck(:name) unless self.groopings.nil?
+        old_tags = current_tags-tags
+        new_tags = tags - current_tags
+    end 
 end 
 
 
