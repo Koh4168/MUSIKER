121ccadc8720424011e08f71b4126a16f76f5579
EC2 Default User <ec2-user@ip-172-31-46-216.ap-northeast-1.compute.internal>
2019-07-13 08:56:47 +0000

    event_groopingモデル　記述

--- db/migrate/20190713084918_create_event_groopings.rb
+++ db/migrate/20190713084918_create_event_groopings.rb
@@ -1,8 +1,12 @@
 class CreateEventGroopings < ActiveRecord::Migration[5.2]
   def change
     create_table :event_groopings do |t|
-
+      t.integer :event_id
+      t.integer :grooping_id
       t.timestamps
     end
+    add_index :event_groopings, :event_id
+    add_index :event_groopings, :grooping_id
+    add_index :event_groopings, [:event_id,:grooping_id],unique: true
   end
 end
