2cab77edc9dc01db62c785f3c62c6cca19288229
EC2 Default User <ec2-user@ip-172-31-46-216.ap-northeast-1.compute.internal>
2019-07-13 09:22:28 +0000

    eventmodel destroy_old_tagsとcreate_new_tags記述

--- app/models/event.rb
+++ app/models/event.rb
@@ -10,6 +10,17 @@ class Event < ApplicationRecord
         old_tags = current_tags-tags
         new_tags = tags - current_tags
     end 
+    
+     # Destroy old taggings:
+    old_tags.each do |old_name|
+         self.groopings.delete Grooping.find_by(name:old_name)
+    end
+    
+     # Create new taggings:
+    new_tags.each do |new_name|
+        event_grooping = Grooping.find_or_create_by(name:new_name)
+        self.groopings << event_grooping
+    end
 end 
 
 
