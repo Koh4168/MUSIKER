e6024a77237633ea877721af3be1bc4b08aa1002
EC2 Default User <ec2-user@ip-172-31-46-216.ap-northeast-1.compute.internal>
2020-01-29 08:57:28 +0000

    データをherokuにアップロードするためにseeds.rb記述

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "csv"
+
+users_csv = CSV.readlines("db/users.csv")
+users_csv.each do |row|
+  User.create(name: row[1],  text: row[2],image: row[3],link: row[4], email: row[7],encrypted_password: row(8),confirmation_token: row(10),unconfirmed_emai: row(13),reset_password_token: row(14))
+end
