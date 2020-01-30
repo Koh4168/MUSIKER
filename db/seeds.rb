2c39138762385d1cb371a999985218fbea93b580
EC2 Default User <ec2-user@ip-172-31-46-216.ap-northeast-1.compute.internal>
2020-01-30 08:35:33 +0000

    add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
-User.create(email: 'tech-camp@gmail.com', password: '12345678', name: 'Mako')
+User.create( name: 'Mako',email:'tech-camp@gmail.com', password: '12345678')
 
-require "csv"
+#require "csv"
 
-users_csv = CSV.readlines("db/users.csv")
-users_csv.each do |row|
-  User.create(name: row[1],  text: row[2],image: row[3],link: row[4], email: row[7],encrypted_password: row(8),confirmation_token: row(10),unconfirmed_emai: row(13),reset_password_token: row(14))
-end
+#users_csv = CSV.readlines("db/users.csv")
+#users_csv.each do |row|
+ # User.create(name: row[1],  text: row[2],image: row[3],link: row[4], email: row[7],encrypted_password: row(8),confirmation_token: row(10),unconfirmed_emai: row(13),reset_password_token: row(14))
+#end
 
 
-users_csv = CSV.readlines("db/users.csv")
-users_csv.each do |row|
-  User.create(name: row[1],  text: row[2],image: row[3],link: row[4], email: row[7],encrypted_password: row(8),confirmation_token: row(10),unconfirmed_emai: row(13),reset_password_token: row(14))
-end
+#users_csv = CSV.readlines("db/users.csv")
+#users_csv.each do |row|
+ # User.create(name: row[1],  text: row[2],image: row[3],link: row[4], email: row[7],encrypted_password: row(8),confirmation_token: row(10),unconfirmed_emai: row(13),reset_password_token: row(14))
+#end
