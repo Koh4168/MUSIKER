b8c5b6093ffb3d85c29af555b938b78c0db340a5
EC2 Default User <ec2-user@ip-172-31-46-216.ap-northeast-1.compute.internal>
2020-02-01 07:14:50 +0000

    add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 Event.create(text: 'tech-camp@gmail.com', name: 'Mako')
 User.create(text: 'tech-camp@gmail.com', name: 'Mako')
 
-#require "csv"
+require "csv"
 
-#users_csv = CSV.readlines("db/users.csv")
-#users_csv.each do |row|
- # User.create(name: row[1],  text: row[2],image: row[3],link: row[4], email: row[7],encrypted_password: row(8),confirmation_token: row(10),unconfirmed_emai: row(13),reset_password_token: row(14))
-#end
+users_csv = CSV.readlines("db/users.csv")
+users_csv.each do |row|
+  User.create(name: row[1],  text: row[2],image: row[3],link: row[4], email: row[7],encrypted_password: row(8),confirmation_token: row(10),unconfirmed_emai: row(13),reset_password_token: row(14))
+end
 
 
 #users_csv = CSV.readlines("db/users.csv")
