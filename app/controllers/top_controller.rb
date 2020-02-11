d85870fd8a0c76826abfe11e213010e10ef46eda
EC2 Default User <ec2-user@ip-172-31-46-216.ap-northeast-1.compute.internal>
2020-02-11 07:51:18 +0000

    toppageに画像表示　flexslider内に収める修正が必要

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,7 @@
 class TopController < ApplicationController
     def index
-        
+          @events = Event.includes(:user).search(params[:search]).order("created_at DESC ").page(params[:page]).per(10)
+        @events = Event.includes(:user).search(params[:search1]).order("created_at DESC ").page(params[:page]).per(10)
     end
     
     def new
