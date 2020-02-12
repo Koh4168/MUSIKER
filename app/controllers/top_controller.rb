77d7c8f9575a0fabcf0afc29663b276eb6a8d897
EC2 Default User <ec2-user@ip-172-31-46-216.ap-northeast-1.compute.internal>
2020-02-12 09:44:55 +0000

    トップページにｕｓｅｒ情報表示

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,7 +1,8 @@
 class TopController < ApplicationController
     def index
-          @events = Event.includes(:user).search(params[:search]).order("created_at DESC ").page(params[:page]).per(10)
+        @events = Event.includes(:user).search(params[:search]).order("created_at DESC ").page(params[:page]).per(10)
         @events = Event.includes(:user).search(params[:search1]).order("created_at DESC ").page(params[:page]).per(10)
+        @user= User.all
     end
     
     def new
